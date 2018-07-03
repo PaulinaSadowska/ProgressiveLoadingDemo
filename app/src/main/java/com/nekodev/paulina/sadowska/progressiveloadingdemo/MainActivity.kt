f7a6e1196bcfac891fcb91d4830f4e9c407556ea
Paulina Sadowska <paulina.sadowska@qpony.pl>
2018-07-03 13:54:22 +0200

    initialize project

---
+++ app/src/main/java/com/nekodev/paulina/sadowska/progressiveloadingdemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nekodev.paulina.sadowska.progressiveloadingdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Picasso.get()
+                .load("http://i.imgur.com/DvpvklR.png")
+                .into(loadedImage)
+    }
+}
